8348f9e2235540ae463f419ddbc59da453780c09
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-12-30 09:57:03 +0100

    replace tabs with spaces

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,19 +6,19 @@ and each box may contain keys to the other boxes.
 """
 
 def canUnlockAll(boxes):
-	"""
-	A method that determines if all the boxes can be opened.
+    """
+    A method that determines if all the boxes can be opened.
 
-	Args:
-		boxes (list of lists): contains list of boxes containing keys
-	Return (bool):
-		True if all boxes can be opened, else return False
-	"""
-	keychain = [0]
-	for key in keychain:
-		for looseKey in boxes[key]:
-			if looseKey not in keychain and looseKey < len(boxes):
-				keychain.append(looseKey)
-	if len(boxes) == len(keychain):
-		return True
-	return False
+    Args:
+        boxes (list of lists): contains list of boxes containing keys
+    Return (bool):
+        True if all boxes can be opened, else return False
+    """
+    keychain = [0]
+    for key in keychain:
+        for looseKey in boxes[key]:
+            if looseKey not in keychain and looseKey < len(boxes):
+                keychain.append(looseKey)
+    if len(boxes) == len(keychain):
+        return True
+    return False
