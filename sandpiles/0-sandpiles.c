e57342bfb997c6c495b13d3c3fd302509c85dc82
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-29 10:19:31 +0100

    Update 0-sandpiles.c

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -92,7 +92,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 
     while (!is_stable(grid1))
     {
-        print_grid(grid1);
+        print_grid_sandpile(grid1);
         toppling_round(grid1);
     }
 }
