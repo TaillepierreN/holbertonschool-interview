d37f4d8e693ed53fa7c7d254eab570ef8a1217f4
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-29 10:23:16 +0100

    Update 0-sandpiles.c

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -30,6 +30,7 @@ void print_grid_sandpile(int grid[3][3])
 int is_stable(int grid[3][3])
 {
 	int i, j;
+
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -57,10 +58,14 @@ void toppling_round(int grid[3][3])
 			if (grid[i][j] > 3)
 			{
 				temp[i][j] -= 4;
-				if (i > 0) temp[i - 1][j] += 1;
-				if (i < 2) temp[i + 1][j] += 1;
-				if (j > 0) temp[i][j - 1] += 1;
-				if (j < 2) temp[i][j + 1] += 1;
+				if (i > 0)
+					temp[i - 1][j] += 1;
+				if (i < 2)
+					temp[i + 1][j] += 1;
+				if (j > 0)
+					temp[i][j - 1] += 1;
+				if (j < 2)
+					temp[i][j + 1] += 1;
 			}
 		}
 	}
