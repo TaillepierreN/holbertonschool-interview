1eabd8d36cafa14d952d15c22fd31312e6d100b5
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-01-29 10:08:32 +0100

    added sandpiles

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef I_DONT_LIKE_SAND
+#define I_DONT_LIKE_SAND
+
+static void print_grid(int grid[3][3]);
+static int is_stable(int grid[3][3]);
+static void toppling_round(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+
+#endif
