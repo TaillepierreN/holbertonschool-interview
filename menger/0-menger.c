9a4ef85ca9e7d50aaf1817c5b154236b96bbdc75
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-03-17 12:20:36 +0100

    Update 0-menger.c

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -14,11 +14,11 @@ int is_blank(int row, int col)
 	while (row > 0 || col > 0)
 	{
 		if (row % 3 == 1 && col % 3 == 1)
-			return 1;
+			return (1);
 		row /= 3;
 		col /= 3;
 	}
-	return 0;
+	return (0);
 }
 
 /**
@@ -28,12 +28,12 @@ int is_blank(int row, int col)
 void menger(int level)
 {
 	int size, row, col;
-	
+
 	if (level < 0)
 		return;
-	
+
 	size = pow(3, level);
-	
+
 	for (row = 0; row < size; row++)
 	{
 		for (col = 0; col < size; col++)
