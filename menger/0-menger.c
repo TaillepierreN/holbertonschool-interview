5ac726b8e0fb91430286c137d3f9b24542db6cdb
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-03-17 12:19:30 +0100

    Update 0-menger.c

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -11,14 +11,14 @@
  */
 int is_blank(int row, int col)
 {
-    while (row > 0 || col > 0)
-    {
-        if (row % 3 == 1 && col % 3 == 1)
-            return 1;
-        row /= 3;
-        col /= 3;
-    }
-    return 0;
+	while (row > 0 || col > 0)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+			return 1;
+		row /= 3;
+		col /= 3;
+	}
+	return 0;
 }
 
 /**
@@ -27,22 +27,22 @@ int is_blank(int row, int col)
  */
 void menger(int level)
 {
-    int size, row, col;
-    
-    if (level < 0)
-        return;
-    
-    size = pow(3, level);
-    
-    for (row = 0; row < size; row++)
-    {
-        for (col = 0; col < size; col++)
-        {
-            if (is_blank(row, col))
-                putchar(' ');
-            else
-                putchar('#');
-        }
-        putchar('\n');
-    }
+	int size, row, col;
+	
+	if (level < 0)
+		return;
+	
+	size = pow(3, level);
+	
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			if (is_blank(row, col))
+				putchar(' ');
+			else
+				putchar('#');
+		}
+		putchar('\n');
+	}
 }
