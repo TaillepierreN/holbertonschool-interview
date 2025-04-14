98e6f7618f136da0b9456a7d74103edc5b0c3ce4
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-04-14 10:34:22 +0200

    Update 0-starwars_characters.js to semistandard

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,31 +1,30 @@
 #!/usr/bin/node
 const request = require('request');
 
-
 const filmID = process.argv[2];
 if (!filmID || isNaN(filmID)) {
   console.log('Invalid film ID, ./0-starwars_characters.js <film_id>');
   process.exit(1);
 }
 
-const url = "https://swapi-api.hbtn.io/api/films/" + filmID;
+const url = 'https://swapi-api.hbtn.io/api/films/' + filmID;
 
 request(url, (error, response, body) => {
-	if (error){
-		console.error('Error: ', error);
-		return;
+	if (error) {
+                console.error('Error: ', error);
+                return;
 	}
 
 	const charData = JSON.parse(body).characters;
 
 	const printCharacters = (index) => {
 		if (index >= charData.length) {
-			return;
+                        return;
 		}
 		request(charData[index], (error, response, body) => {
 			if (error) {
-				console.error('Error: ', error);
-				return;
+                                console.error('Error: ', error);
+                                return;
 			}
 			console.log(JSON.parse(body).name);
 			printCharacters(index + 1);
