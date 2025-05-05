ee948fedb2729aa0514d290c3b8fba5cca832fae
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-05-05 09:01:49 +0200

    update

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -7,5 +7,4 @@ int advanced_binary(int *array, size_t size, int value);
 int binary_search(int *array, size_t high, size_t low, int value);
 void print_array(int *array, size_t high, size_t low);
 
-
 #endif
