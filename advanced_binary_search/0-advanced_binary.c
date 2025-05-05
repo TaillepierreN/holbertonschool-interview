74c1c73d3979d6c3609ba7499e1d66c61b85fe75
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-05-05 08:52:32 +0200

    Update 0-advanced_binary.c

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -11,7 +11,7 @@
  */
 int *_advanced_binary(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
 	if (array == NULL || size == 0)
 		return (NULL);
@@ -25,13 +25,13 @@ int *_advanced_binary(int *array, size_t size, int value)
 	}
 	printf("\n");
 
-	i = size / 2;
+	i = (size - 1) / 2;
 
 	if (array[i] == value)
 	{
 		if (i == 0 || array[i - 1] != value)
 			return (array + i);
-		return (_advanced_binary(array, i + 1, value));
+		return (_advanced_binary(array, i, value));
 	}
 	else if (array[i] > value)
 	{
@@ -57,5 +57,5 @@ int advanced_binary(int *array, size_t size, int value)
 
 	if (res == NULL)
 		return (-1);
-	return (res - array);
+	return ((int)(res - array));
 }
