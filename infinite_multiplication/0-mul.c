999941621ad0d7f14ecc22e0eca33687d0f02231
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-05-29 09:45:34 +0200

    betty

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -9,15 +9,15 @@
  */
 int is_digit(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
@@ -28,11 +28,11 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (s[len])
-        len++;
-    return (len);
+	while (s[len])
+		len++;
+	return (len);
 }
 
 /**
@@ -42,19 +42,19 @@ int _strlen(char *s)
  */
 void print_result(int *result, int len)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < len && result[i] == 0)
-        i++;
+	while (i < len && result[i] == 0)
+		i++;
 
-    if (i == len)
-        _putchar('0');
-    else
-    {
-        for (; i < len; i++)
-            _putchar(result[i] + '0');
-    }
-    _putchar('\n');
+	if (i == len)
+		_putchar('0');
+	else
+	{
+		for (; i < len; i++)
+			_putchar(result[i] + '0');
+	}
+	_putchar('\n');
 }
 
 /**
@@ -66,40 +66,40 @@ void print_result(int *result, int len)
  */
 int main(int argc, char *argv[])
 {
-    char *num1, *num2;
-    int len1, len2, i, j, mul, sum, *result;
+	char *num1, *num2;
+	int len1, len2, i, j, mul, sum, *result;
 
-    if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-    {
-        _putchar('E'); _putchar('r'); _putchar('r'); _putchar('o');
-        _putchar('r'); _putchar('\n');
-        exit(98);
-    }
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+	{
+		_putchar('E'); _putchar('r'); _putchar('r'); _putchar('o');
+		_putchar('r'); _putchar('\n');
+		exit(98);
+	}
 
-    num1 = argv[1];
-    num2 = argv[2];
-    len1 = _strlen(num1);
-    len2 = _strlen(num2);
+	num1 = argv[1];
+	num2 = argv[2];
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
 
-    result = malloc(sizeof(int) * (len1 + len2));
-    if (!result)
-        exit(98);
+	result = malloc(sizeof(int) * (len1 + len2));
+	if (!result)
+		exit(98);
 
-    for (i = 0; i < len1 + len2; i++)
-        result[i] = 0;
+	for (i = 0; i < len1 + len2; i++)
+		result[i] = 0;
 
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            mul = (num1[i] - '0') * (num2[j] - '0');
-            sum = result[i + j + 1] + mul;
-            result[i + j + 1] = sum % 10;
-            result[i + j] += sum / 10;
-        }
-    }
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			mul = (num1[i] - '0') * (num2[j] - '0');
+			sum = result[i + j + 1] + mul;
+			result[i + j + 1] = sum % 10;
+			result[i + j] += sum / 10;
+		}
+	}
 
-    print_result(result, len1 + len2);
-    free(result);
-    return (0);
+	print_result(result, len1 + len2);
+	free(result);
+	return (0);
 }
