07ba7f02228c87ae14826c5ec42e2b9968523c85
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-06-23 10:04:17 +0200

    use dynamic programming

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -4,19 +4,20 @@ determine the fewest number of coins needed to meet a given amount total
 """
 
 def makeChange(coins, total):
-	"""Determine the fewest number of coins needed to meet a given amount total.
-	
-	Args:
-		coins (list): List of coin denominations.
-		total (int): The total amount to make change for.
-	"""
-	if total <= 0:
-		return 0
-	coins.sort(reverse=True)
-	coinCount = 0;
+    """Determine the fewest number of coins needed to meet a given amount total.
+    
+    Args:
+        coins (list): List of coin denominations.
+        total (int): The total amount to make change for.
+    """
+    if total <= 0:
+        return 0
 
-	for coin in coins:
-		while total >= coin:
-			total -= coin
-			coinCount += 1
-	return coinCount if total == 0 else -1
+    minAmount = [float('inf')] * (total + 1)
+    minAmount[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            minAmount[i] = min(minAmount[i], minAmount[i - coin] + 1)
+
+    return minAmount[total] if minAmount[total] != float('inf') else -1
